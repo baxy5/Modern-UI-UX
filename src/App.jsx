11dc021f61455ea8987c59f3ef818a204ef53123
baxy5 <bakos.soda@gmail.com>
2022-08-16 12:12:26 +0200

    structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,46 @@
-import { useState } from "react";
-import "./App.css";
+import React from "react";
+import styles from "./style";
 
-function App() {
-  const [count, setCount] = useState(0);
+import {
+  Navbar,
+  Hero,
+  Stats,
+  Business,
+  Billing,
+  CardDeal,
+  Testimonials,
+  Clients,
+  CTA,
+  Footer,
+} from "./components/";
 
-  return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello</h1>
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
     </div>
-  );
-}
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart} ${styles.paddingX}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
+      </div>
+    </div>
+  </div>
+);
 
 export default App;
